af5d3d52d8ee64694dfef51c6f5d3863980abcdf
kirisaki-vk <kirisaki.vk@gmail.com>
2024-07-27 18:11:14 +0300

    test: PatrimoineMapperTest

---
+++ src/test/java/com/harena/api/conf/service/mappers/PatrimoineMapperTest.java
@@ -0,0 +1,33 @@
+package com.harena.api.conf.service.mappers;
+
+import static java.time.Month.JUNE;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.harena.api.service.mappers.Mapper;
+import com.harena.api.service.mappers.PatrimoineMapper;
+import java.time.LocalDate;
+import java.util.Set;
+import org.junit.jupiter.api.Test;
+import school.hei.patrimoine.modele.Patrimoine;
+import school.hei.patrimoine.modele.Personne;
+
+public class PatrimoineMapperTest {
+  @Test
+  void patrimoine_mapper_test() {
+    Mapper<Patrimoine, com.harena.api.endpoint.rest.model.Patrimoine> subject =
+        new PatrimoineMapper();
+    LocalDate date = LocalDate.of(2024, JUNE, 26);
+    Patrimoine patrimoine1 =
+        new Patrimoine("Patrimoine 1", new Personne("Personne 1"), date, Set.of());
+
+    com.harena.api.endpoint.rest.model.Patrimoine patrimoine2 =
+        new com.harena.api.endpoint.rest.model.Patrimoine()
+            .nom(patrimoine1.nom())
+            .t(date)
+            .possesseur(new com.harena.api.endpoint.rest.model.Personne().nom("Personne 1"))
+            .valeurComptable(patrimoine1.getValeurComptable());
+
+    assertEquals(patrimoine2, subject.toRestModel(patrimoine1));
+    assertEquals(patrimoine1, subject.toObjectModel(patrimoine2));
+  }
+}
