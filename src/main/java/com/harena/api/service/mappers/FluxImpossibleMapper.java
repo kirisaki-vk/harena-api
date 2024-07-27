cb9da81a97ca11b5edfc3cc2561ce28362d28c26
kirisaki-vk <kirisaki.vk@gmail.com>
2024-07-27 21:56:20 +0300

    feat: flux impossible mapper

---
+++ src/main/java/com/harena/api/service/mappers/FluxImpossibleMapper.java
@@ -0,0 +1,36 @@
+package com.harena.api.service.mappers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import school.hei.patrimoine.modele.FluxImpossibles;
+
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+@Component
+@RequiredArgsConstructor
+public class FluxImpossibleMapper implements Mapper<FluxImpossibles, com.harena.api.endpoint.rest.model.FluxImpossibles> {
+  private final FluxAgentMapper fluxAgentMapper;
+
+  @Override
+  public com.harena.api.endpoint.rest.model.FluxImpossibles toRestModel(FluxImpossibles objectModel) {
+    return new com.harena.api.endpoint.rest.model.FluxImpossibles()
+        .fluxArgents(objectModel.flux().stream().map(fluxAgentMapper::toRestModel).toList())
+        .date(objectModel.date())
+        .nomArgent(objectModel.nomArgent())
+        .valeurArgent(objectModel.valeurArgent());
+  }
+
+  @Override
+  public FluxImpossibles toObjectModel(com.harena.api.endpoint.rest.model.FluxImpossibles restModel) {
+    return new FluxImpossibles(
+        restModel.getDate(),
+        restModel.getNomArgent(),
+        Objects.requireNonNull(restModel.getValeurArgent()),
+        Objects.requireNonNull(
+            restModel.getFluxArgents()).stream()
+            .map(fluxAgentMapper::toObjectModel)
+            .collect(Collectors.toSet())
+    );
+  }
+}
