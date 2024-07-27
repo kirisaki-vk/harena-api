9a41da8421314d1bd2f7242c6f7a97c4a1657578
Abi-Nf <hei.abigail.3@gmail.com>
2024-07-27 12:21:10 +0300

    feat: add FluxAgentMapper

---
+++ src/main/java/com/harena/api/service/mappers/FluxAgentMapper.java
@@ -0,0 +1,33 @@
+package com.harena.api.service.mappers;
+
+import school.hei.patrimoine.modele.possession.FluxArgent;
+
+class FluxAgentMapper implements Mapper<FluxArgent, com.harena.api.endpoint.rest.model.FluxArgent> {
+  @Override
+  public com.harena.api.endpoint.rest.model.FluxArgent toRestModel(FluxArgent objectModel) {
+    var fluxArgent = new com.harena.api.endpoint.rest.model.FluxArgent();
+    fluxArgent.setNom(objectModel.getNom());
+    fluxArgent.setT(objectModel.getT());
+    fluxArgent.setDebut(objectModel.getDebut());
+    fluxArgent.setFin(objectModel.getFin());
+    fluxArgent.setDevise(new DeviseMapper().toRestModel(objectModel.getDevise()));
+    fluxArgent.setArgent(new ArgentMapper().toRestModel(objectModel.getArgent()));
+    fluxArgent.setDateDOperation(objectModel.getDateOperation());
+    fluxArgent.setValeurComptable(objectModel.getValeurComptable());
+    fluxArgent.setFluxMensuel(objectModel.getFluxMensuel());
+    return fluxArgent;
+  }
+
+  @Override
+  public FluxArgent toObjectModel(com.harena.api.endpoint.rest.model.FluxArgent restModel) {
+    return new FluxArgent(
+      restModel.getNom(),
+      new ArgentMapper().toObjectModel(restModel.getArgent()),
+      restModel.getDebut(),
+      restModel.getFin(),
+      restModel.getFluxMensuel(),
+      restModel.getDateDOperation(),
+      new DeviseMapper().toObjectModel(restModel.getDevise())
+    );
+  }
+}
