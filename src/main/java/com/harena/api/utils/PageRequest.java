85f77f41a7d6f4a95187d080c551f3a1cf793f77
kirisaki-vk <kirisaki.vk@gmail.com>
2024-07-27 18:09:03 +0300

    chore: add static method factory for PageRequest

--- src/main/java/com/harena/api/utils/PageRequest.java
+++ src/main/java/com/harena/api/utils/PageRequest.java
@@ -1,3 +1,7 @@
 package com.harena.api.utils;
 
-public record PageRequest(int pageNumber, int pageSize) {}
+public record PageRequest(int pageNumber, int pageSize) {
+  public static PageRequest of(int pageNumber, int pageSize) {
+    return new PageRequest(pageNumber, pageSize);
+  }
+}
